f7c20a12423ac06a058f04eb0154093cb7b0cf49
Abdul Haqani <35286145+abdulhaqani@users.noreply.github.com>
2019-05-17 12:35:23 -0400

    Mongo db
    set up mongo db
    created a model
    created a schema (called 'green')
    set up a basic insert into the db (see login routing)
    Need to set up the front end submitting etc

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
+/* eslint-disable import/no-absolute-path */
 // Defining global consts for the webserver
 const express = require('express');
+const mongoose = require('mongoose');
 const handlebars = require('express-handlebars').create({
   defaultLayout: 'main',
 });
+const path = require('path');
+
+// require models
+require('./models/Greenhouse');
+
+// model instance
+const Green = mongoose.model('green');
+
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+
+// connect to mongoose
+mongoose
+  .connect('mongodb://127.0.0.1/greenhouse-dev', {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('connected to mongodb'))
+  .catch(err => console.log(err));
+
+mongoose.connection
+  .once('open', () => {
+    console.log('connected to greenhouse-dev');
+  })
+  .on('error', error => {
+    console.log('connection error');
+  });
 
 const app = express();
 app.engine('handlebars', handlebars.engine);
-const path = require('path');
 
 app.use(express.static('public'));
 app.set('view engine', 'handlebars');
@@ -20,6 +47,13 @@ app.get('/', (req, res) => {
 // Login routing
 app.get('/home', (req, res) => {
   res.render('home');
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+  new Green(newUser).save().then(() => {
+    res.redirect('/');
+  });
 });
 
 // Register routing
