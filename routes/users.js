c1ca73d99d61d085c542fbad24dd8247bbc27ab2
silvernitrateion <russell@silvernitrateion.com>
2019-05-17 16:33:31 -0400

    fixed variables

--- routes/users.js
+++ routes/users.js
@@ -29,52 +29,52 @@ router.post('/login', (req, res, next) => {
   })(req, res, next);
 });
 // register form post
-// router.post('/register', (req, res) => {
-//   const errors = [];
-
-//   if (req.body.password != req.body.password2) {
-//     errors.push({ text: 'Passwords do not match' });
-//   }
-//   if (req.body.password.length < 4) {
-//     errors.push({ text: 'Password must be at least 4 characters' });
-//   }
-//   if (errors.length > 0) {
-//     res.render('users/register', {
-//       errors,
-//       name: req.body.name,
-//       email: req.body.email,
-//       password: req.body.password,
-//       password2: req.body.password2,
-//     });
-//   } else {
-//     User.findOne({ email: req.body.email }).then(user => {
-//       if (user) {
-//         req.flash('error_msg', 'email already registered');
-//         res.redirect('/users/register');
-//       } else {
-//         const newUser = new User({
-//           name: req.body.name,
-//           email: req.body.email,
-//           password: req.body.password,
-//         });
-//         bcrypt.genSalt(10, (err, salt) => {
-//           bcrypt.hash(newUser.password, salt, (err, hash) => {
-//             if (err) throw err;
-//             newUser.password = hash;
-//             newUser
-//               .save()
-//               .then(() => {
-//                 req.flash('success_msg', 'you are now registered');
-//                 res.redirect('/users/login');
-//               })
-//               .catch(err => {
-//                 console.log(err);
-//               });
-//           });
-//         });
-//       }
-//     });
-//   }
-// });
+router.post('/register', (req, res) => {
+  const errors = [];
+  if (req.body.passwd1 != req.body.passwd2) {
+    errors.push({ text: 'Passwords do not match' });
+  }
+  if (req.body.passwd1.length < 4) {
+    errors.push({ text: 'Password must be at least 4 characters' });
+  if (errors.length > 0) {
+    res.render('users/register', {
+      errors,
+      firstName: req.body.fname,
+      lastName: req.body.lname,  
+      email: req.body.email,
+      password: req.body.passwd1,
+      password2: req.body.passwd2,
+    });
+  } else {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      req.flash('error_msg', 'email already registered');
+      res.redirect('/users/register');
+        } else {
+        const newUser = new User({
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        password: req.body.passwd,
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+          .save()
+          .then(() => {
+            req.flash('success_msg', 'you are now registered');
+            res.redirect('/users/login');
+          })
+          .catch(err => {
+            console.log(err);
+          });
+          });
+      });
+    }
+  });
+  }
+});
 
 module.exports = router;
