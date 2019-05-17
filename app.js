fe3d4a01d3bdaad77dcce521937efc45a67bd1b1
Abdul Haqani <35286145+abdulhaqani@users.noreply.github.com>
2019-05-17 10:53:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-// Defining global vars for the webserver
-var express = require('express');
-var handlebars = require('express-handlebars')
+// Defining global consts for the webserver
+const express = require('express');
+const handlebars = require('express-handlebars')
     .create({ defaultLayout: 'main'});
-var app = express();
+const app = express();
 app.engine('handlebars', handlebars.engine);
-var path = require('path');
+const path = require('path');
 app.use(express.static('public'))
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 // Home page routing
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
     res.render('login');
 });
 
 // Login routing
-app.get('/home', function(req, res){
+app.get('/home', (req, res) => {
     res.render('home')
 });
 
 // Register routing
-app.get('/register', function(req, res){
+app.get('/register', (req, res) => {
     res.render('register')
 });
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), () => {
     console.log('-------------------------------------------')
     console.log('==> Node Webserver Started on Port 3000 <==')
     console.log('-------------------------------------------')
