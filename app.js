202ecf9f7c0fde66385b4e514e96873cef2306e5
Abdul Haqani <35286145+abdulhaqani@users.noreply.github.com>
2019-05-17 11:27:41 -0400

    Config Auto formatting

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
 // Defining global consts for the webserver
 const express = require('express');
-const handlebars = require('express-handlebars')
-    .create({ defaultLayout: 'main'});
+const handlebars = require('express-handlebars').create({
+  defaultLayout: 'main',
+});
+
 const app = express();
 app.engine('handlebars', handlebars.engine);
 const path = require('path');
-app.use(express.static('public'))
+
+app.use(express.static('public'));
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 // Home page routing
 app.get('/', (req, res) => {
-    res.render('login');
+  res.render('login');
 });
 
 // Login routing
 app.get('/home', (req, res) => {
-    res.render('home')
+  res.render('home');
 });
 
 // Register routing
 app.get('/register', (req, res) => {
-    res.render('register')
+  res.render('register');
 });
 
 app.listen(app.get('port'), () => {
-    console.log('-------------------------------------------')
-    console.log('==> Node Webserver Started on Port 3000 <==')
-    console.log('-------------------------------------------')
+  console.log('-------------------------------------------');
+  console.log('==> Node Webserver Started on Port 3000 <==');
+  console.log('-------------------------------------------');
 });
