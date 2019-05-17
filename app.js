ab6ef55589af6563de75539d5e2d1e7ebcae2036
Abdul Haqani <35286145+abdulhaqani@users.noreply.github.com>
2019-05-17 15:56:02 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,60 @@
-//
+/* eslint-disable import/no-absolute-path */
+// Defining global consts for the webserver
+const express = require('express');
+const mongoose = require('mongoose');
+const handlebars = require('express-handlebars').create({
+  defaultLayout: 'main',
+});
+const path = require('path');
+
+// load routes
+const greenhouse = require('./routes/greenhouse');
+const users = require('./routes/users');
+
+// require models
+require('./models/Greenhouse');
+
+// model instance
+const Green = mongoose.model('green');
+
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+
+// connect to mongoose
+mongoose
+  .connect('mongodb://127.0.0.1/greenhouse-dev', {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('connected to mongodb'))
+  .catch(err => console.log(err));
+
+mongoose.connection
+  .once('open', () => {
+    console.log('connected to greenhouse-dev');
+  })
+  .on('error', error => {
+    console.log('connection error');
+  });
+
+const app = express();
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+
+// Home page routing
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+// use routes
+// app.use('/greenhouse', greenhouse);
+app.use('/users', users);
+
+app.listen(app.get('port'), () => {
+  console.log('-------------------------------------------');
+  console.log('==> Node Webserver Started on Port 3000 <==');
+  console.log('-------------------------------------------');
+});
