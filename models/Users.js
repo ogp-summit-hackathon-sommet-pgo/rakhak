f66e25dc18d5e4493a869f33ab3386deef572950
silvernitrateion <russell@silvernitrateion.com>
2019-05-21 11:29:09 -0400

    Modified users schema, fixed broken template

--- models/Users.js
+++ models/Users.js
@@ -1,20 +1,37 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
-
+/* User Schama
+ * Schema for users, required
+ * fields are Organization,
+ * email, password. Non-required
+ * fields are First and Lastname.
+ */
 const UserSchema = new Schema({
-  name: {
+
+  organization: {
     type: String,
-    required: true,
+    require: true,
   },
+
+  firstName: {
+    type: String,
+  },
+
+  lastName: {
+    type: String,
+  },
+
   email: {
     type: String,
     required: true,
   },
+
   password: {
     type: String,
     required: true,
   },
+
   date: {
     type: Date,
     default: Date.now,
